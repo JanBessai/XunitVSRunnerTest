85374bf569bb2ec81c6470374ee29d59ff18c91c
LS14\bessai <jan.bessai@tu-dortmund.de>
2015-03-10 12:59:20 +0100

    Working version

---
+++ XunitVSRunnerTest/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace XunitVSRunnerTest
+{
+    public class Class1
+    {
+        [Fact]
+        bool test_Test()
+        {
+            return true;
+        }
+    }
+}
